5d2d81f830bd53e15cc435ccef5b346622b352e3
Pedrvicente <pedro12gil@gmail.com>
2026-07-01 17:24:35 +0100

    draw maze and start of setting positions

--- interface/app.py
+++ interface/app.py
@@ -1,6 +1,7 @@
 import pygame
 from contracts import Direction
 from engine.game import PacmanGame
+from config.parser import Parser
 
 
 class APP:
@@ -15,18 +16,43 @@ class APP:
         self.fps = 60
         self.run = True
 
+    def draw_maze(self, grid: list[list[int]]):
+        CELL_H = self.HEIGHT/len(grid)
+        CELL_W = self.WIDTH/len(grid[0])
+        for i in range(len(grid)):
+            y = i * CELL_H
+            for j in range(len(grid[i])):
+                x = j * CELL_W
+                pygame.draw.rect(self.WIN, 'black', (x, y, CELL_W, CELL_H))
+
+                # parede norte
+                if not (grid[i][j] & 1):
+                    pygame.draw.line(self.WIN, 'white', (x, y), (x + CELL_W, y), 1)
+
+                # parede este
+                if not (grid[i][j] & 2):
+                    pygame.draw.line(self.WIN, 'white', (x + CELL_W, y), (x + CELL_W, y + CELL_H), 1)
+
+                # parede sul
+                if not (grid[i][j] & 4):
+                    pygame.draw.line(self.WIN, 'white', (x, y + CELL_H), (x + CELL_W, y + CELL_H), 1)
+
+                # parede oeste
+                if not (grid[i][j] & 8):
+                    pygame.draw.line(self.WIN, 'white', (x, y), (x, y + CELL_H), 1)
+
+    def draw_pacman(self, grid: list[list[int]], pacman_pos: tuple[int, int]):
+        CELL_H = self.HEIGHT/len(grid)
+        CELL_W = self.WIDTH/len(grid[0])
+        x = pacman_pos[0] * CELL_W + (CELL_W/2)
+        y = pacman_pos[1] * CELL_H + (CELL_H/2)
+        pygame.draw.circle(self.WIN, 'blue', (x, y), 5)
+
     def run_game(self):
         while self.run:
             dt = self.timer.tick(self.fps)/1000
             direction = None
             self.WIN.fill("black")
-
-            CELL = 10
-            for i in range(0, 5):
-                y = i * CELL
-                for j in range(0, 5):
-                    x = j * CELL
-                    pygame.draw.rect(self.WIN, 'white', (x, y, 5, 5))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -40,11 +66,18 @@ class APP:
                         direction = Direction.LEFT
                     if event.key == pygame.K_d:
                         direction = Direction.RIGHT
-            self.game.tick(direction, dt)
+            maze = self.game.tick(direction, dt)
+            grid = maze.maze
+            pacman_pos = maze.pacman_pos
+            self.draw_maze(grid)
+            self.draw_pacman(grid, pacman_pos)
             pygame.display.flip()
         pygame.quit()
 
 
 if __name__ == '__main__':
-    game = APP()
+    config = Parser()
+    parse = config.run_parsing()
+    pacman = PacmanGame(parse)
+    game = APP(pacman)
     game.run_game()
